8f313f3eec3f964c71307d205f7ced8c8f558f43
Sarankumar V <sarankumar1311@gmail.com>
2024-08-23 09:43:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let d=Math.floor(Math.random()*100);
+let g=1;
+function fun1(){
+   let a=document.getElementById("num"); 
+if(a.value==d&&g<=10){
+   g++;
+   window.location.href="./won.html";  
+}
+else  if(a.value>=d && g<=10){
+   document.getElementById("count").innerHTML=g;
+  
+
+       document.getElementById("hint").innerHTML="Try with lower value";
+       g++;
+    }
+else  if(a.value<=d && g<=10){
+        document.getElementById("hint").innerHTML="Try with greater value";
+        document.getElementById("count").innerHTML=g;
+        g++;
+     }
+else if(g>10){
+   window.location.href="./loss.html";
+}
+   }
+function fun2(){
+   location.reload();
+}
